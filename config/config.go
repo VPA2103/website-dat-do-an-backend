7aa1127b62663d46fdaaa0674c811cd631ac2b4c
thangvu123321 <vu713194@gmail.com>
2026-06-01 22:11:14 +0700

    sua connect db

--- config/config.go
+++ config/config.go
@@ -13,21 +13,21 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDB() {
-	dsn := os.Getenv("DB_URL")
+// func ConnectDB() {
+// 	dsn := os.Getenv("DB_URL")
 
-	if dsn == "" {
-		panic("❌ DB_URL không tồn tại! Hãy kiểm tra Variables trong Railway.")
-	}
+// 	if dsn == "" {
+// 		panic("❌ DB_URL không tồn tại! Hãy kiểm tra Variables trong Railway.")
+// 	}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(fmt.Sprintf("❌ Failed to connect to database: %v", err))
-	}
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		panic(fmt.Sprintf("❌ Failed to connect to database: %v", err))
+// 	}
 
-	DB = db
-	fmt.Println("🚀 Database connected successfully")
-}
+// 	DB = db
+// 	fmt.Println("🚀 Database connected successfully")
+// }
 
 func SetupCORS(r *gin.Engine) {
 	r.Use(cors.New(cors.Config{
@@ -41,29 +41,29 @@ func SetupCORS(r *gin.Engine) {
 	}))
 }
 
-// func ConnectDB() {
-// 	host := os.Getenv("POSTGRES_HOST")
-// 	port := os.Getenv("POSTGRES_PORT")
-// 	user := os.Getenv("POSTGRES_USER")
-// 	password := os.Getenv("POSTGRES_PASSWORD")
-// 	dbname := os.Getenv("POSTGRES_DBNAME")
-// 	sslmode := os.Getenv("POSTGRES_SSLMODE")
+func ConnectDB() {
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DBNAME")
+	sslmode := os.Getenv("POSTGRES_SSLMODE")
 
-// 	// kiểm tra thiếu env
-// 	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
-// 		panic("❌ Thiếu biến môi trường Postgres")
-// 	}
+	// kiểm tra thiếu env
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" {
+		panic("❌ Thiếu biến môi trường Postgres")
+	}
 
-// 	dsn := fmt.Sprintf(
-// 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Ho_Chi_Minh",
-// 		host, port, user, password, dbname, sslmode,
-// 	)
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Ho_Chi_Minh",
+		host, port, user, password, dbname, sslmode,
+	)
 
-// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		panic(fmt.Sprintf("❌ Failed to connect to database: %v", err))
-// 	}
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("❌ Failed to connect to database: %v", err))
+	}
 
-// 	DB = db
-// 	fmt.Println("🚀 Database connected successfully")
-// }
+	DB = db
+	fmt.Println("🚀 Database connected successfully")
+}
